b98f3db9462e0a93bc538155c23559b066efa53e
max <golchanskiy23@gmail.com>
2025-04-06 08:52:11 +0300

    Completed downloader

--- downloader.go
+++ downloader.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"github.com/avast/retry-go"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/spf13/pflag"
 	"os"
 )
 
@@ -18,11 +24,26 @@ type Downloader struct {
 }
 
 func Configurate() (ConfigurationStructure, error) {
+	bucketFlag := pflag.String("bucket", "", "S3 bucket name")
+	nameFlag := pflag.String("name", "", "S3 downloader name")
+	fileFlag := pflag.String("file", "", "File to download")
+	pflag.Parse()
+
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2"),
+	},
+	)
+	downloader := s3manager.NewDownloader(sess)
+	f, err := os.Create(*fileFlag)
+	if err != nil {
+		return ConfigurationStructure{}, fmt.Errorf("failed to create file %s, %v", *fileFlag, err)
+	}
+
 	return ConfigurationStructure{
-		client: nil,
-		bucket: "",
-		name:   "",
-		file:   nil,
+		client: downloader,
+		bucket: *bucketFlag,
+		name:   *nameFlag,
+		file:   f,
 	}, nil
 }
 
@@ -34,5 +55,20 @@ func NewDownloader(client *s3manager.Downloader, numBytes uint) *Downloader {
 }
 
 func (d *Downloader) Download(file *os.File, bucket, name string) (uint, error) {
-	return 0, nil
+	d.numBytes = 0
+	si3 := &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(name),
+	}
+
+	err := retry.Do(func() error {
+		n, err1 := d.client.Download(file, si3)
+		if err1 != nil {
+			return err1
+		}
+		d.numBytes = uint(n)
+		return err1
+	})
+
+	return d.numBytes, err
 }
